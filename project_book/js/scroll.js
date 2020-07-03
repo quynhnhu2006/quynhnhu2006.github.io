45c4764457e6485a1bd9496e455a0e13eab05a62
webdemo <nhu.nguyen.20694@gmail.com>
2020-07-03 11:30:29 +0900

    0307

--- project_book/js/scroll.js
+++ project_book/js/scroll.js
@@ -26,32 +26,94 @@ $(function(){
 	});
 	
 	// lat sach//
-	$(window).ready(function() {
-		$('#flipbook').turn({
-			 				width:1000,
-			                height:650,
-							autoCenter: true,
-							display: 'double',
-							acceleration: true,
-							gradients: !$.isTouch,
-							elevation:50,
-			                direction: 'ltr',
-							when: {
-								turned: function(e, page) {
-									/*console.log('Current view: ', $(this).turn('view'));*/
+	//$(window).ready(function() {
+			/*$('#flipbook').turn({
+								width:1000,
+								height:650,
+								autoCenter: true,
+								display: 'double',
+								acceleration: true,
+								gradients: !$.isTouch,
+								elevation:50,
+								direction: 'ltr',
+								/*when: {
+									turned: function(e, page) {
+										console.log('Current view: ', $(this).turn('view'));*/
+									/*}
 								}
-							}
-						});
+			});*/
+		/*$('#flipbook').turn({
+			width: 1000,
+			height: 650,
+			duration: 1000,
+			autoCenter: true,
+			gradients: true,
+			elevation: 50,
+			direction: 'ltr',
+		});
+    	//change from single to double page
+      	$(window).width(function(){
+			  var win = $(this); //this = window
+			  if (win.width() >= 480) { $('#flipbook').turn('display','double'); console.log ("run double !!"); }
+			  else {
+				$('#flipbook').turn({
+				  width:300,
+				  height:400,
+				  display: 'single',
+			  });
+				  	console.log ("run single !!");
+			  }
+		});
+		$(window).resize(function(){
+			var win = $(this); //this = window
+			if (win.width() >= 480) { $('#flipbook').turn('display','double');}
+			else {
+			  $('#flipbook').turn({
+				  width:300,
+				  height:400,
+				  display: 'single',
+			  });
+			}
+		});*/
+/* turn js */
+	if($('#flipbook').length) {
+		if(window.innerWidth>480) {
+			   $('#flipbook').turn({
+				width:1000,
+				height:650,
+				autoCenter: true,
+				display: 'double',
+				acceleration: true,
+				inclination:50,
+				gradients: true,
+				elevation:50,
+				direction: 'ltr',
+				zoom:2,// you can change it as you desire
+				   duration:50,
+			  });
+         }else {
+			 $('#flipbook').turn({
+				width:300,
+				height:400,
+				elevation:50,
+				inclination:50,
+				display: 'single',
+				autocenter:true,
+				acceleration: true,
+				gradients:true,
+				zoom:2,// you can change it as you desire
+				   duration:50,
+			  });
+			 
+		 }
+		
 		$('#prevpage').click(function(){
         	$('#flipbook').turn('previous');
     	});
  
 		$('#nextpage').click(function(){
 			$('#flipbook').turn('next');
-		});
-	});
-	
-	
+		});	
 	$(window).bind('keydown', function(e){		
 		if (e.keyCode==37)
 			$('#flipbook').turn('previous');
@@ -59,4 +121,20 @@ $(function(){
 			$('#flipbook').turn('next');
 			
 	});
+}	
+	//parallax
+	$(window).scroll(function(){
+				var windowHeight = $(window).height();
+				var topWindow = $(window).scrollTop();
+				var blockAni = $('.parallax');
+				blockAni.each(function(){
+					var targetPosition = $(this).offset().top;
+					if(topWindow + windowHeight > targetPosition + 100){
+						$(this).addClass("css_show");
+					}
+					else{
+						$(this).removeClass("css_show");
+					}
+				});
+	});		
 });
